6648e8c0d2cb1539cd93bc220ae07470d0dba2ce
Jono Mallanyk <jono@mallanyk.com>
2013-12-04 19:01:07 -0500

    Added Community and Groups layout. WIP: search result data.

---
+++ client/views/community_and_groups/search_data.js
@@ -0,0 +1,42 @@
+var searchData = [
+  {
+    avatar: 'avatar.jpg',
+    name: 'Nicolas Cage',
+    country: 'United States',
+    tags: 'Actor, Scientist'
+  },
+  {
+    avatar: 'avatar.jpg',
+    name: 'Nicolas Cage',
+    country: 'United States',
+    tags: 'Actor, Scientist'
+  },
+  {
+    avatar: 'avatar.jpg',
+    name: 'Nicolas Cage',
+    country: 'United States',
+    tags: 'Actor, Scientist'
+  },
+  {
+    avatar: 'avatar.jpg',
+    name: 'Nicolas Cage',
+    country: 'United States',
+    tags: 'Actor, Scientist'
+  },
+  {
+    avatar: 'avatar.jpg',
+    name: 'Nicolas Cage',
+    country: 'United States',
+    tags: 'Actor, Scientist'
+  },
+  {
+    avatar: 'avatar.jpg',
+    name: 'Nicolas Cage',
+    country: 'United States',
+    tags: 'Actor, Scientist'
+  }
+];
+
+Template.communityAndGroups.helpers({
+  searchResult: searchData
+});
